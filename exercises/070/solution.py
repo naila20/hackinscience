07de7b33916c50f659787db5b6007b94da56270d
naila20 <naila.haddadi@cri-paris.org>
2015-11-10 16:03:29 +0100

    Print every two letters pairs, without duplicates

--- exercises/070/solution.py
+++ exercises/070/solution.py
@@ -2,6 +2,6 @@ import string
 for x in string.ascii_lowercase:
     for y in string.ascii_lowercase:
         if x == y:
-            print()
+            pass
         else:
             print(x + y)
