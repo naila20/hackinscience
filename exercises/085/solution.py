c25246c1ae5344825781a51094478d4667c54336
naila20 <naila.haddadi@cri-paris.org>
2015-11-17 22:13:56 +0100

    sort_students

---
+++ exercises/085/solution.py
@@ -0,0 +1,15 @@
+def sort_a_list(l):
+    return sorted(l, key=int, reverse=True)
+
+
+
+from operator import itemgetter
+def sort_by_mark(my_class):
+    for i in sorted(my_class, key=itemgetter(0), reverse=True):
+        print(i)
+
+
+from operator import itemgetter
+def sort_by_name(my_class):
+    for i in sorted(my_class, key=itemgetter(1)):
+        print(i)
