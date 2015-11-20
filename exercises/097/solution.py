887f389f79a8700d4f494a922631a9921f331225
naila20 <naila.haddadi@cri-paris.org>
2015-11-20 18:05:26 +0100

    Sets of love

--- exercises/097/solution.py
+++ exercises/097/solution.py
@@ -9,11 +9,11 @@ def love_meet(bob, alice):
 
 
 def affair_meet(bob, alice, silvester):
-     list = []
-     for i in bob:
-         for j in alice:
-             for k in silvester:
-                 if i != j and j == k:
+    list = []
+    for i in bob:
+        for j in alice:
+            for k in silvester:
+                if i != j and j == k:
                     list.append(j)
                     list2 = set(list)
-     return sorted(list2)
+    return sorted(list2)
